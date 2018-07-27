9578b38a1ecf6a2c9dddfe2df9c5d83d69ddbd6c
Odomontois <odomontois@gmail.com>
2018-07-27 18:07:53 +0300

    Talk preparation

--- src/main/scala/ru/tinkoff/tagless/functor/AccountsModule.scala
+++ src/main/scala/ru/tinkoff/tagless/functor/AccountsModule.scala
@@ -6,24 +6,24 @@ import org.http4s.dsl.Http4sDsl
 import cats.syntax.flatMap._
 import cats.syntax.applicativeError._
 import org.http4s.dsl.impl.QueryParamDecoderMatcher
+import ru.tinkoff.tagless.functor.AccountsModule.DiffMatcher
 
 final case class AccountsModule[F[_]: Sync](accounts: Accounts[F])(implicit dsl: Http4sDsl[F]) {
   import dsl._
 
-
-
-
   val service: HttpRoutes[F] = HttpRoutes
     .of[F] {
       case GET -> Root / "account" / id => accounts.get(id).flatMap(bd => Ok(bd.toString))
-      case POST -> Root / "account" / id :? IntPara => accounts.get(id).flatMap(bd => Ok(bd.toString))
-      case _                            => Ok("lol")
+      case POST -> Root / "account" / id :? DiffMatcher(diff) =>
+        accounts.modify(id, diff).flatMap(bd => Ok(bd.toString))
+      case PUT -> Root / "account" / id =>  accounts.create(id).flatMap(_ => Ok())
     }
     .mapF(_.recoverWith { case AccountError.DoesNotExist(id) => OptionT.liftF(NotFound(s"account $id not found")) })
 }
 
-object AccountsModule{
+object AccountsModule {
   object DiffMatcher extends QueryParamDecoderMatcher[BigDecimal]("diff")
 
-  implicit val bigDecimalQueryParameter: QueryParamDecoder[BigDecimal] = QueryParamDecoder.fromUnsafeCast()
+  implicit val bigDecimalQueryParameter: QueryParamDecoder[BigDecimal] =
+    QueryParamDecoder.fromUnsafeCast(v => BigDecimal(v.value))("BigDecimal")
 }
